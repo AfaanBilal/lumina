8870f96cac0aade4fd1b71251f25a7f3cf3c4a7e
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 14:48:46 +0530

    ✨ Link hover

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center justify-between p-2">
         <h1 class="text-2xl">Lumina</h1>
-        <a href="https://afaan.dev" target="_blank" rel="noopener" class="text-lg text-slate-500 hover:underline">&copy; Afaan Bilal</a>
+        <a href="https://afaan.dev" target="_blank" rel="noopener" class="text-lg text-slate-500 hover:text-blue-500">&copy; Afaan Bilal</a>
     </div>
     <LuminaSheet :sheet="store.sheet" />
 </template>
