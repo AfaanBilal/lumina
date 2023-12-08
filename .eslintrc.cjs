6de9a2888d158d03dacebcb4dd0daa41e0b0fad7
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 09:24:49 +0530

    🚨 Setup ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+    root: true,
+    // https://eslint.org/docs/rules/no-undef#nodejs
+    env: {
+        node: true,
+        // https://eslint.vuejs.org/user-guide/#compiler-macros-such-as-defineprops-and-defineemits-generate-no-undef-warnings
+        "vue/setup-compiler-macros": true,
+    },
+    parser: "vue-eslint-parser",
+    // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-recommended",
+    ],
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)",
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+    rules: {
+        "vue/singleline-html-element-content-newline": "off",
+        "vue/max-attributes-per-line": "off",
+        "vue/html-indent": [
+            "error",
+            4
+        ],
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "quotes": [
+            "error",
+            "double",
+            { "avoidEscape": true }
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "eol-last": [
+            "error",
+            "always"
+        ],
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
+};
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
 const count = ref(0);
 </script>
 
 <template>
     <h1 class="text-3xl">{{ msg }}</h1>
+
     <button type="button" @click="count++">count is {{ count }}</button>
 </template>
