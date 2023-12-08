2421a0dc200d816228bf3b6c9dfb5de06d00e3c6
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 16:35:43 +0530

    ✨ Auto focus input on click

--- src/components/LuminaCell.vue
+++ src/components/LuminaCell.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="flex flex-shrink-0 items-center w-20 h-6 border p-0.5" :class="{ 'border-2 border-blue-500': isSelected }" @click="store.selectCell(rowIndex, index)">
-        <div v-show="!isSelected">{{ cell.value }}</div>
+    <div class="flex flex-shrink-0 items-center w-20 h-6 border p-0.5" :class="{ 'border-2 border-blue-500': isSelected }" @click="cellClick">
+        <div v-show="!isSelected" class="w-full overflow-clip">{{ cell.value }}</div>
         <div v-if="isSelected">
-            <input v-model="value" type="text" class="w-full h-5 outline-none" @keyup.enter="store.selectCellDown()">
+            <input ref="input" v-model="value" type="text" class="w-full h-5 outline-none" @keyup.enter="store.selectCellDown()">
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, nextTick, ref } from "vue";
 import { useStore } from "../store/store";
 import type { ILuminaCell, TLuminaCellValue } from "../App.d";
 
@@ -26,4 +26,10 @@ const value = computed<TLuminaCellValue>({
         store.updateCell(props.rowIndex, props.index, { ...props.cell, value: v });
     },
 });
+
+const input = ref<HTMLInputElement | null>(null);
+const cellClick = () => {
+    store.selectCell(props.rowIndex, props.index);
+    nextTick(() => input.value?.focus());
+};
 </script>
