8b31bcd201622b6e93f048351420dbe7f1223b85
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 16:26:35 +0530

    ✨ Highlight active row / col

--- src/components/LuminaIndexCell.vue
+++ src/components/LuminaIndexCell.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="flex items-center justify-center font-semibold flex-shrink-0 w-10 h-6 border p-0.5 bg-slate-100">
+    <div class="flex items-center justify-center font-semibold flex-shrink-0 w-10 h-6 border p-0.5 bg-slate-100" :class="{ 'bg-slate-200': index === store.activeCell.rowIndex }">
         {{ index + 1 }}
     </div>
 </template>
 
 <script setup lang="ts">
+import { useStore } from "../store/store";
+
 defineProps<{ index: number }>();
+
+const store = useStore();
 </script>
