5f414a3c4ae9ff5186f13d4fada21a202124a063
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 16:45:36 +0530

    ✨ Header / Index: medium

--- src/components/LuminaIndexCell.vue
+++ src/components/LuminaIndexCell.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-center font-semibold flex-shrink-0 w-10 h-6 border p-0.5 bg-slate-100" :class="{ 'bg-slate-200': index === store.activeCell.rowIndex }">
+    <div class="flex items-center justify-center font-medium flex-shrink-0 w-10 h-6 border p-0.5 bg-slate-100" :class="{ 'bg-slate-200': index === store.activeCell.rowIndex }">
         {{ index + 1 }}
     </div>
 </template>
