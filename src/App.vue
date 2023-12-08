d7bf7650c62945860f269a9e6517d013dd0480df
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 14:48:21 +0530

    ✨ Font

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Lumina</h1>
+    <div class="flex items-center justify-between p-2">
+        <h1 class="text-2xl">Lumina</h1>
+        <a href="https://afaan.dev" target="_blank" rel="noopener" class="text-lg text-slate-500 hover:underline">&copy; Afaan Bilal</a>
+    </div>
     <LuminaSheet :sheet="store.sheet" />
 </template>
 
