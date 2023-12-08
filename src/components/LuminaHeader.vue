e53dd1bed770f60f6606e13b12db4c15d5387837
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 15:05:10 +0530

    ✨ Unfix add column

--- src/components/LuminaHeader.vue
+++ src/components/LuminaHeader.vue
@@ -4,7 +4,7 @@
         <div v-for="i in store.maxColumns" :key="i" class="flex flex-shrink-0 items-center justify-center font-semibold w-20 h-6 border p-0.5 bg-slate-100">
             {{ indexToColumn(i - 1) }}
         </div>
-        <button class="fixed right-0 flex items-center justify-center h-6 px-2 bg-slate-300" @click="store.addColumn()">
+        <button class="flex items-center justify-center h-6 px-2 bg-slate-300" @click="store.addColumn()">
             <IconPlus :size="16" />
         </button>
     </div>
