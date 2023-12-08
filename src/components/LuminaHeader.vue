a23f33788ecaa8eda8aa253740cf450a474cd3a5
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 14:27:17 +0530

    ✨ Add Column

--- src/components/LuminaHeader.vue
+++ src/components/LuminaHeader.vue
@@ -4,10 +4,14 @@
         <div v-for="i in store.maxColumns" :key="i" class="flex items-center justify-center font-semibold w-20 h-6 border p-0.5 bg-slate-100">
             {{ indexToColumn(i - 1) }}
         </div>
+        <button class="flex items-center justify-center px-4 bg-slate-300" @click="store.addColumn()">
+            <IconPlus :size="16" />
+        </button>
     </div>
 </template>
 
 <script setup lang="ts">
+import { IconPlus } from "@tabler/icons-vue";
 import { useStore } from "../store/store";
 import { indexToColumn } from "../utils/helpers";
 
--- src/components/LuminaRow.vue
+++ src/components/LuminaRow.vue
@@ -2,17 +2,13 @@
     <div class="flex items-center w-full">
         <LuminaIndexCell :index="index" />
         <LuminaCell v-for="(cell, i) of row.cells" :key="cell.id" :cell="cell" :index="i" :row-index="index" />
-        <button @click="store.addColumn()">Add Column</button>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { ILuminaRow } from "../App.d";
-import LuminaCell from "./LuminaCell.vue";
-import { useStore } from "../store/store";
 import LuminaIndexCell from "./LuminaIndexCell.vue";
+import LuminaCell from "./LuminaCell.vue";
 
 defineProps<{ index: number; row: ILuminaRow; }>();
-
-const store = useStore();
 </script>
