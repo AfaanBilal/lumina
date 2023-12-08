0590bfb4ebaa8cadaf55556037f59d6c0545b878
Afaan Bilal <afaanbilal@gmail.com>
2023-12-08 16:50:44 +0530

    ✨ Add row at index

--- src/components/LuminaIndexCell.vue
+++ src/components/LuminaIndexCell.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="flex items-center justify-center font-medium text-sm flex-shrink-0 w-10 h-6 border p-0.5 bg-slate-100" :class="{ 'bg-slate-200': index === store.activeCell.rowIndex }">
+    <div class="relative flex items-center justify-center font-medium text-sm flex-shrink-0 w-10 h-6 border p-0.5 bg-slate-100 group" :class="{ 'bg-slate-200': index === store.activeCell.rowIndex }">
+        <button class="absolute items-center justify-center p-0.5 top-1 bg-slate-300 hidden group-hover:flex" title="Add a row above" @click="store.addRow(index-1)">
+            <IconPlus :size="12" />
+        </button>
+
         {{ index + 1 }}
     </div>
 </template>
 
 <script setup lang="ts">
+import { IconPlus } from "@tabler/icons-vue";
 import { useStore } from "../store/store";
 
 defineProps<{ index: number }>();
