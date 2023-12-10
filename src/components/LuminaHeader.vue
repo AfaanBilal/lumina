f4ca2858329b93599d5e400e4748089f9fbbedb5
Afaan Bilal <afaanbilal@gmail.com>
2023-12-10 08:20:03 +0530

    ✨ Top-Left cell: lighter border

--- src/components/LuminaHeader.vue
+++ src/components/LuminaHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border-b-4 border-r-4 border-slate-400 bg-slate-200">&nbsp;</div>
+    <div class="border-b-4 border-r-4 border-slate-300 bg-slate-200">&nbsp;</div>
     <div v-for="i in store.maxColumns" :key="i" class="relative text-sm flex flex-shrink-0 items-center justify-center font-medium w-20 h-6 border p-0.5 bg-slate-100 group" :class="{ 'bg-slate-200': i - 1 === store.activeCell.cellIndex }">
         <button class="absolute items-center justify-center p-0.5 left-1 bg-slate-300 hidden group-hover:flex" title="Add a colum to the left" @click="store.addColumn(i-1)">
             <IconPlus :size="12" />
