303378be299d630d78ed07976a271235e873a58d
Afaan Bilal <afaanbilal@gmail.com>
2023-12-11 08:24:29 +0530

    🎨 Extract: Toolbar

--- src/App.vue
+++ src/App.vue
@@ -8,96 +8,15 @@
             <a href="https://afaan.dev" target="_blank" rel="noopener"
                 class="text-lg text-slate-500 hover:text-blue-500">&copy; Afaan Bilal</a>
         </div>
-        <div class="flex gap-4 p-1 text-xs">
-            <div class="flex border rounded">
-                <div class="flex items-center justify-center w-6 border-r rounded-l cursor-pointer"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.bold }" @click="toggleBold">
-                    <IconBold :size="16" />
-                </div>
-                <div class="flex items-center justify-center w-6 border-r cursor-pointer"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.italic }" @click="setItalic">
-                    <IconItalic :size="16" />
-                </div>
-                <div class="flex items-center justify-center w-6 rounded-r cursor-pointer"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.underline }" @click="toggleUnderline">
-                    <IconUnderline :size="16" />
-                </div>
-            </div>
-            <div class="flex border rounded">
-                <div class="flex items-center justify-center w-6 border-r rounded-l cursor-pointer"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'left' }"
-                    @click="setTextAlign('left')">
-                    <IconAlignLeft :size="16" />
-                </div>
-                <div class="flex items-center justify-center w-6 border-r cursor-pointer"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'center' }"
-                    @click="setTextAlign('center')">
-                    <IconAlignCenter :size="16" />
-                </div>
-                <div class="flex items-center justify-center w-6 rounded-r cursor-pointer"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'right' }"
-                    @click="setTextAlign('right')">
-                    <IconAlignRight :size="16" />
-                </div>
-            </div>
-            <div class="flex items-center justify-center border rounded">
-                <input type="number" min="8" max="100" :value="store.ActiveCell.style?.fontSize || 11"
-                    class="outline-none p-0.5 w-10" @change="setFontSize">
-            </div>
-            <div class="flex border">
-                <div class="flex items-center justify-center w-6 border-r">
-                    <color-picker shape="circle" :round-history="true" @pure-color-change="setBackgroundColor" />
-                </div>
-                <div class="flex items-center justify-center w-6">
-                    <color-picker shape="circle" :round-history="true" @pure-color-change="setTextColor" />
-                </div>
-            </div>
-            <div class="flex items-center gap-4 ml-auto">
-                <div class="flex items-center gap-1">
-                    <input id="stripes" v-model="stripes" type="checkbox">
-                    <label for="stripes" class="cursor-pointer select-none">Stripes</label>
-                </div>
-                <div class="flex items-center gap-1">
-                    <input id="autofocus" v-model="autofocus" type="checkbox">
-                    <label for="autofocus" class="cursor-pointer select-none">Auto focus input</label>
-                </div>
-                <div class="flex items-center gap-1">
-                    <input id="rowBand" v-model="rowBand" type="checkbox">
-                    <label for="rowBand" class="cursor-pointer select-none">Row band</label>
-                </div>
-                <div class="flex items-center gap-1">
-                    <input id="colBand" v-model="colBand" type="checkbox">
-                    <label for="colBand" class="cursor-pointer select-none">Column band</label>
-                </div>
-            </div>
-        </div>
+        <LuminaToolbar />
         <LuminaSheet :sheet="store.sheet" />
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import { useStore } from "./store/store";
 import LuminaSheet from "./components/LuminaSheet.vue";
-import { IconBold } from "@tabler/icons-vue";
-import { IconItalic } from "@tabler/icons-vue";
-import { IconUnderline } from "@tabler/icons-vue";
-import { IconAlignLeft } from "@tabler/icons-vue";
-import { IconAlignCenter } from "@tabler/icons-vue";
-import { IconAlignRight } from "@tabler/icons-vue";
+import LuminaToolbar from "./components/LuminaToolbar.vue";
 
 const store = useStore();
-
-const autofocus = computed<boolean>({ get() { return store.settings.autofocus; }, set(v: boolean) { store.updateSettings("autofocus", v); } });
-const stripes = computed<boolean>({ get() { return store.settings.stripes; }, set(v: boolean) { store.updateSettings("stripes", v); } });
-const rowBand = computed<boolean>({ get() { return store.settings.rowBand; }, set(v: boolean) { store.updateSettings("rowBand", v); } });
-const colBand = computed<boolean>({ get() { return store.settings.colBand; }, set(v: boolean) { store.updateSettings("colBand", v); } });
-
-const setFontSize = (e: Event) => store.updateStyle({ fontSize: parseInt((e.target as HTMLInputElement).value) });
-const setTextAlign = (alignment: "left" | "center" | "right") => store.updateStyle({ textAlignment: alignment });
-const toggleBold = () => store.updateStyle({ bold: !store.ActiveCell.style?.bold });
-const setItalic = () => store.updateStyle({ italic: !store.ActiveCell.style?.italic });
-const toggleUnderline = () => store.updateStyle({ underline: !store.ActiveCell.style?.underline });
-const setBackgroundColor = (color: string) => store.updateStyle({ backgroundColor: color });
-const setTextColor = (color: string) => store.updateStyle({ textColor: color });
 </script>
