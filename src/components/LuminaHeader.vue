9d2e5c4f0bab27871861b62b11a2928604296a26
Afaan Bilal <afaanbilal@gmail.com>
2023-12-13 07:59:49 +0530

    ✨ Row / Col add buttons

--- src/components/LuminaHeader.vue
+++ src/components/LuminaHeader.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="border-b-4 border-r-4 cursor-pointer border-slate-300 bg-slate-200"
-        :class="{ 'bg-blue-100 border-blue-600': store.isWholeSheetSelected }"
-        @click="store.selectSheet()">
+        :class="{ 'bg-blue-100 border-blue-600': store.isWholeSheetSelected }" @click="store.selectSheet()">
         &nbsp;
     </div>
     <div v-for="i in store.maxColumns" :key="i"
-        class="relative text-sm flex flex-shrink-0 items-center justify-center font-medium w-20 h-6 border p-0.5 bg-slate-100 group cursor-pointer"
+        class="relative text-sm flex flex-shrink-0 items-center justify-center font-medium h-6 border p-0.5 bg-slate-100 group cursor-pointer select-none"
         :class="{
             'bg-slate-200': i - 1 === store.activeCell.cellIndex,
             'bg-blue-100 border-x-blue-700 border-t-blue-700': store.selectedCells.start.cellIndex === i - 1 && store.selectedCells.end.cellIndex === i - 1
         }" @click="store.selectColumn(i - 1)">
-        <button class="absolute items-center justify-center p-0.5 left-1 bg-slate-300 hidden group-hover:flex"
+        <button class="absolute items-center justify-center p-0.5 left-1 bg-slate-300 hidden rounded group-hover:flex"
             title="Add a colum to the left" @click="store.addColumn(i - 1)">
             <IconPlus :size="12" />
         </button>
 
         {{ indexToColumn(i - 1) }}
+
+        <div class="absolute right-0 hidden cursor-col-resize group-hover:flex text-slate-500">|</div>
     </div>
 </template>
 
--- src/components/LuminaIndexCell.vue
+++ src/components/LuminaIndexCell.vue
@@ -2,9 +2,10 @@
     <div class="relative flex items-center justify-center font-medium text-sm border p-0.5 bg-slate-100 group select-none cursor-pointer"
         :class="{ 'bg-slate-200': index === store.activeCell.rowIndex, 'bg-blue-100 border-l-blue-700 border-y-blue-700': isRowSelected }"
         @click="store.selectRow(index)">
-        <button class="absolute top-0 left-0 items-center justify-center hidden bg-slate-300 group-hover:flex"
+        <button
+            class="absolute top-0 left-0 items-center justify-center hidden rounded-sm bg-slate-300 group-hover:flex p-0.5"
             title="Add a row above" @click="store.addRow(index - 1)">
-            <IconPlus :size="10" />
+            <IconPlus :size="8" />
         </button>
 
         {{ index + 1 }}
