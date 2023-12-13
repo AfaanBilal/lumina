9dc8af227ac440587be3bf3948ab3c9900ca3e3d
Afaan Bilal <afaanbilal@gmail.com>
2023-12-13 14:31:28 +0530

    ✨ Custom Function: AVG

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,11 +2,13 @@ import { Parser } from "expr-eval";
 
 export const parser = new Parser();
 
-parser.functions.sum = (...v: Array<number>) => v.reduce((a, v) => a + v);
-
 parser.consts = {
     pi: Math.PI,
     e: Math.E,
     true: true,
     false: false,
 };
+
+parser.functions.sum = (...v: Array<number>) => v.reduce((a, v) => a + v);
+
+parser.functions.avg = (...v: Array<number>) => v.reduce((a, v) => a + v) / v.length;
