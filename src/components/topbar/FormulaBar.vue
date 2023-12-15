6aec56c20fdf1a70f870e53e2add2221ecfcc093
Afaan Bilal <afaanbilal@gmail.com>
2023-12-15 15:25:17 +0530

    ✨ Formula bar: vertical alignment

--- src/components/topbar/FormulaBar.vue
+++ src/components/topbar/FormulaBar.vue
@@ -3,13 +3,13 @@
         <div class="flex items-center justify-center w-10 p-1 font-mono font-semibold border-r border-y">
             {{ store.ActiveCellName }}
         </div>
-        <div class="px-2 py-1 border-y">
+        <div class="flex items-center justify-center px-2 py-1 border-y">
             <IconMathFunction :size="18" />
         </div>
         <input v-model="activeCellValue" type="text"
             class="self-stretch flex-1 p-1 border outline-none bg-slate-100 focus:border-slate-400"
             @keyup.enter="store.setActiveCellDown()">
-        <div v-if="isFormula(activeCellValue)" class="p-1 border-y bg-slate-50">
+        <div v-if="isFormula(activeCellValue)" class="flex items-center justify-center p-1 border-y bg-slate-50">
             <IconEqual :size="18" />
         </div>
         <div v-if="isFormula(activeCellValue)" class="flex-1 p-1 font-semibold border border-l-0 bg-slate-50">
