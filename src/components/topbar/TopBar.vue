c95e7a2c9c231eccad002922d3f6d717ce19d7a7
Afaan Bilal <afaanbilal@gmail.com>
2023-12-15 16:23:53 +0530

    ✨ Top-right links

--- src/components/topbar/TopBar.vue
+++ src/components/topbar/TopBar.vue
@@ -5,11 +5,15 @@
                 <img src="/logo.png" alt="logo" class="w-8">
                 <h1 class="text-2xl font-semibold">Lumina</h1>
             </div>
-            <div class="flex flex-col items-end">
-                <a href="https://afaan.dev" target="_blank" rel="noopener"
-                    class="text-sm text-slate-500 hover:text-blue-500">&copy; Afaan Bilal</a>
+            <div class="flex self-start gap-2 text-sm">
                 <a href="https://github.com/AfaanBilal/lumina" target="_blank" rel="noopener"
-                    class="text-sm text-slate-500 hover:text-blue-500">&rarr; GitHub</a>
+                    class="flex items-center gap-1 pr-2 border-r border-r-slate-300 text-slate-500 hover:text-blue-500">
+                    <IconBrandGithub :size="16" /> GitHub
+                </a>
+                <a href="https://afaan.dev" target="_blank" rel="noopener"
+                    class="flex items-center gap-1 text-slate-500 hover:text-blue-500">
+                    <IconCopyright :size="16" /> Afaan Bilal
+                </a>
             </div>
         </div>
         <Toolbar />
@@ -27,5 +31,6 @@
  * @copyright   2023 Afaan Bilal
  */
 
+import { IconCopyright, IconBrandGithub } from "@tabler/icons-vue";
 import Toolbar from "./Toolbar.vue";
 </script>
