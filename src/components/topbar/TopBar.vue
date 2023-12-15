dc52f3d1ad160f53fb5c9578ab00103f4022ec07
Afaan Bilal <afaanbilal@gmail.com>
2023-12-15 15:58:35 +0530

    ✨ GitHub link

--- src/components/topbar/TopBar.vue
+++ src/components/topbar/TopBar.vue
@@ -5,8 +5,12 @@
                 <img src="/logo.png" alt="logo" class="w-8">
                 <h1 class="text-2xl font-semibold">Lumina</h1>
             </div>
-            <a href="https://afaan.dev" target="_blank" rel="noopener"
-                class="self-start text-sm text-slate-500 hover:text-blue-500">&copy; Afaan Bilal</a>
+            <div class="flex flex-col items-end">
+                <a href="https://afaan.dev" target="_blank" rel="noopener"
+                    class="text-sm text-slate-500 hover:text-blue-500">&copy; Afaan Bilal</a>
+                <a href="https://github.com/AfaanBilal/lumina" target="_blank" rel="noopener"
+                    class="text-sm text-slate-500 hover:text-blue-500">&rarr; GitHub</a>
+            </div>
         </div>
         <Toolbar />
     </div>
