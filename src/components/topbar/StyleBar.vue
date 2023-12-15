035cb0b099b91b35b9ed212edf2daf0305376a65
Afaan Bilal <afaanbilal@gmail.com>
2023-12-15 11:37:42 +0530

    ♻️  Extract: StyleBar

---
+++ src/components/topbar/StyleBar.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="flex gap-2">
+        <div class="flex">
+            <div class="flex items-center justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.bold }" title="Bold" @click="toggleBold">
+                <IconBold :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 border-r cursor-pointer border-y hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.italic }" title="Italic" @click="setItalic">
+                <IconItalic :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 cursor-pointer border-y hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.underline }" title="Underline"
+                @click="toggleUnderline">
+                <IconUnderline :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.strikethrough }" title="Strikethrough"
+                @click="toggleStrikethrough">
+                <IconStrikethrough :size="18" />
+            </div>
+        </div>
+        <div class="flex">
+            <div class="flex items-center justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'left' }"
+                title="Text align left" @click="setTextAlign('left')">
+                <IconAlignLeft :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 cursor-pointer border-y hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'center' }"
+                title="Text align center" @click="setTextAlign('center')">
+                <IconAlignCenter :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'right' }"
+                title="Text align right" @click="setTextAlign('right')">
+                <IconAlignRight :size="18" />
+            </div>
+        </div>
+        <div class="flex">
+            <div class="flex items-center justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.verticalAlignment === 'top' }"
+                title="Vertical align top" @click="setVerticalAlign('top')">
+                <IconLayoutAlignTop :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 cursor-pointer border-y hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.verticalAlignment === 'middle' }"
+                title="Vertical align middle" @click="setVerticalAlign('middle')">
+                <IconLayoutAlignCenter :size="18" />
+            </div>
+            <div class="flex items-center justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-200"
+                :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.verticalAlignment === 'bottom' }"
+                title="Vertical align bottom" @click="setVerticalAlign('bottom')">
+                <IconLayoutAlignBottom :size="18" />
+            </div>
+        </div>
+        <div class="flex items-center justify-center">
+            <div class="flex items-center self-stretch justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-100"
+                title="Decrease font size"
+                @click="store.updateStyle({ fontSize: (store.ActiveCell.style?.fontSize || 11) - 1 })">
+                <IconMinus :size="18" />
+            </div>
+            <input type="number" min="8" max="200" :value="store.ActiveCell.style?.fontSize || 11"
+                class="outline-none text-center p-0.5 w-6 border-y [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                title="Font size" @change="setFontSize">
+            <div class="flex items-center self-stretch justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-100"
+                title="Increase font size"
+                @click="store.updateStyle({ fontSize: (store.ActiveCell.style?.fontSize || 11) + 1 })">
+                <IconPlus :size="18" />
+            </div>
+        </div>
+        <div class="flex">
+            <div class="flex items-center gap-1 px-1 border rounded-l hover:bg-slate-100" title="Background color">
+                <IconBucketDroplet :size="18" />
+                <color-picker v-model:pure-color="backgroundColor" shape="circle" :round-history="true"
+                    @pure-color-change="setBackgroundColor" />
+            </div>
+            <div class="flex items-center gap-1 px-1 border border-l-0 rounded-r hover:bg-slate-100" title="Text color">
+                <IconTextColor :size="18" />
+                <color-picker v-model:pure-color="textColor" shape="circle" :round-history="true"
+                    @pure-color-change="setTextColor" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import {
+    IconBold, IconItalic, IconUnderline,
+    IconAlignLeft, IconAlignCenter, IconAlignRight, IconStrikethrough,
+    IconLayoutAlignTop, IconLayoutAlignCenter, IconLayoutAlignBottom,
+    IconMinus, IconPlus, IconBucketDroplet, IconTextColor,
+} from "@tabler/icons-vue";
+import { useStore } from "../../store/store";
+
+const store = useStore();
+
+const setFontSize = (e: Event) => store.updateStyle({ fontSize: parseInt((e.target as HTMLInputElement).value) });
+const setTextAlign = (alignment: "left" | "center" | "right") => store.updateStyle({ textAlignment: alignment });
+const setVerticalAlign = (alignment: "top" | "middle" | "bottom") => store.updateStyle({ verticalAlignment: alignment });
+const toggleBold = () => store.updateStyle({ bold: !store.ActiveCell.style?.bold });
+const toggleStrikethrough = () => store.updateStyle({ strikethrough: !store.ActiveCell.style?.strikethrough });
+const setItalic = () => store.updateStyle({ italic: !store.ActiveCell.style?.italic });
+const toggleUnderline = () => store.updateStyle({ underline: !store.ActiveCell.style?.underline });
+
+const backgroundColor = computed({ get() { return store.ActiveCell.style?.backgroundColor || "rgb(255,255,255)"; }, set() {} });
+const textColor = computed({ get() { return store.ActiveCell.style?.textColor || "rgb(0,0,0)"; }, set() {} });
+const setBackgroundColor = (color: string) => store.updateStyle({ backgroundColor: color });
+const setTextColor = (color: string) => store.updateStyle({ textColor: color });
+</script>
--- src/components/topbar/Toolbar.vue
+++ src/components/topbar/Toolbar.vue
@@ -95,107 +95,19 @@
                 </div>
             </Dropdown>
         </div>
-        <div class="flex gap-2">
-            <div class="flex">
-                <div class="flex items-center justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.bold }" title="Bold" @click="toggleBold">
-                    <IconBold :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 border-r cursor-pointer border-y hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.italic }" title="Italic"
-                    @click="setItalic">
-                    <IconItalic :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 cursor-pointer border-y hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.underline }" title="Underline"
-                    @click="toggleUnderline">
-                    <IconUnderline :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.strikethrough }" title="Strikethrough"
-                    @click="toggleStrikethrough">
-                    <IconStrikethrough :size="18" />
-                </div>
-            </div>
-            <div class="flex">
-                <div class="flex items-center justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'left' }"
-                    title="Text align left" @click="setTextAlign('left')">
-                    <IconAlignLeft :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 cursor-pointer border-y hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'center' }"
-                    title="Text align center" @click="setTextAlign('center')">
-                    <IconAlignCenter :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.textAlignment === 'right' }"
-                    title="Text align right" @click="setTextAlign('right')">
-                    <IconAlignRight :size="18" />
-                </div>
-            </div>
-            <div class="flex">
-                <div class="flex items-center justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.verticalAlignment === 'top' }"
-                    title="Vertical align top" @click="setVerticalAlign('top')">
-                    <IconLayoutAlignTop :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 cursor-pointer border-y hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.verticalAlignment === 'middle' }"
-                    title="Vertical align middle" @click="setVerticalAlign('middle')">
-                    <IconLayoutAlignCenter :size="18" />
-                </div>
-                <div class="flex items-center justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-200"
-                    :class="{ 'bg-slate-800 text-white': store.ActiveCell.style?.verticalAlignment === 'bottom' }"
-                    title="Vertical align bottom" @click="setVerticalAlign('bottom')">
-                    <IconLayoutAlignBottom :size="18" />
-                </div>
-            </div>
-            <div class="flex items-center justify-center">
-                <div class="flex items-center self-stretch justify-center w-6 border rounded-l cursor-pointer hover:bg-slate-100"
-                    title="Decrease font size"
-                    @click="store.updateStyle({ fontSize: (store.ActiveCell.style?.fontSize || 11) - 1 })">
-                    <IconMinus :size="18" />
-                </div>
-                <input type="number" min="8" max="200" :value="store.ActiveCell.style?.fontSize || 11"
-                    class="outline-none text-center p-0.5 w-6 border-y [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                    title="Font size" @change="setFontSize">
-                <div class="flex items-center self-stretch justify-center w-6 border rounded-r cursor-pointer hover:bg-slate-100"
-                    title="Increase font size"
-                    @click="store.updateStyle({ fontSize: (store.ActiveCell.style?.fontSize || 11) + 1 })">
-                    <IconPlus :size="18" />
-                </div>
-            </div>
-            <div class="flex">
-                <div class="flex items-center gap-1 px-1 border rounded-l hover:bg-slate-100" title="Background color">
-                    <IconBucketDroplet :size="18" />
-                    <color-picker v-model:pure-color="backgroundColor" shape="circle" :round-history="true"
-                        @pure-color-change="setBackgroundColor" />
-                </div>
-                <div class="flex items-center gap-1 px-1 border border-l-0 rounded-r hover:bg-slate-100" title="Text color">
-                    <IconTextColor :size="18" />
-                    <color-picker v-model:pure-color="textColor" shape="circle" :round-history="true"
-                        @pure-color-change="setTextColor" />
-                </div>
-            </div>
-        </div>
+        <StyleBar />
         <FormulaBar />
     </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import {
-    IconBold, IconItalic, IconUnderline,
-    IconAlignLeft, IconAlignCenter, IconAlignRight, IconStrikethrough,
-    IconLayoutAlignTop, IconLayoutAlignCenter, IconLayoutAlignBottom,
-    IconDeviceFloppy, IconFile, IconSettings, IconPrinter,
-    IconMinus, IconPlus, IconBucketDroplet, IconTextColor, IconHelp,
-} from "@tabler/icons-vue";
+import { IconDeviceFloppy, IconFile, IconSettings, IconPrinter, IconHelp } from "@tabler/icons-vue";
 import { useStore } from "../../store/store";
 import { download } from "../../utils/helpers";
 import Dropdown from "v-dropdown";
 import FormulaBar from "./FormulaBar.vue";
+import StyleBar from "./StyleBar.vue";
 
 const store = useStore();
 
@@ -204,19 +116,6 @@ const stripes = computed<boolean>({ get() { return store.settings.stripes; }, se
 const rowBand = computed<boolean>({ get() { return store.settings.rowBand; }, set(v: boolean) { store.updateSettings("rowBand", v); } });
 const colBand = computed<boolean>({ get() { return store.settings.colBand; }, set(v: boolean) { store.updateSettings("colBand", v); } });
 const referenceVisible = computed<boolean>({ get() { return store.settings.referenceVisible; }, set(v: boolean) { store.updateSettings("referenceVisible", v); } });
-
-const setFontSize = (e: Event) => store.updateStyle({ fontSize: parseInt((e.target as HTMLInputElement).value) });
-const setTextAlign = (alignment: "left" | "center" | "right") => store.updateStyle({ textAlignment: alignment });
-const setVerticalAlign = (alignment: "top" | "middle" | "bottom") => store.updateStyle({ verticalAlignment: alignment });
-const toggleBold = () => store.updateStyle({ bold: !store.ActiveCell.style?.bold });
-const toggleStrikethrough = () => store.updateStyle({ strikethrough: !store.ActiveCell.style?.strikethrough });
-const setItalic = () => store.updateStyle({ italic: !store.ActiveCell.style?.italic });
-const toggleUnderline = () => store.updateStyle({ underline: !store.ActiveCell.style?.underline });
-
-const backgroundColor = computed({ get() { return store.ActiveCell.style?.backgroundColor || "rgb(255,255,255)"; }, set() {} });
-const textColor = computed({ get() { return store.ActiveCell.style?.textColor || "rgb(0,0,0)"; }, set() {} });
-const setBackgroundColor = (color: string) => store.updateStyle({ backgroundColor: color });
-const setTextColor = (color: string) => store.updateStyle({ textColor: color });
 
 const fileDropdown = ref();
 const input = ref<HTMLInputElement>();
