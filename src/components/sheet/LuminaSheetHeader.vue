0aee43e6c1f191b6e9e59f3c319c6d43146be133
Afaan Bilal <afaanbilal@gmail.com>
2023-12-17 21:23:03 +0530

    ✨ Header: index

--- src/components/sheet/LuminaSheetHeader.vue
+++ src/components/sheet/LuminaSheetHeader.vue
@@ -3,21 +3,21 @@
         :class="{ 'bg-blue-100 border-blue-600': store.isSheetSelected }" @click="store.selectSheet()">
         &nbsp;
     </div>
-    <div v-for="i in store.columnCount" :key="i"
+    <div v-for="(_, i) in store.columnCount" :key="i"
         class="text-sm flex flex-shrink-0 items-center justify-center font-medium h-6 border p-0.5 bg-slate-100 group cursor-pointer select-none sticky top-0"
         :class="{
-            'bg-slate-200': i - 1 === store.activeCellCoordinates.cellIndex,
-            'bg-blue-300': store.selectedCells.start.cellIndex <= i - 1 && store.selectedCells.end.cellIndex >= i - 1,
-            'bg-slate-300 z-50': store.sheet.style.cols?.[i - 1]?.frozen,
-        }" :style="(store.sheet.style.cols?.[i - 1]?.frozen ? `left: ${store.getFrozenLeft(i - 1)}px;` : '')"
-        @click="store.selectColumn(i - 1)">
+            'bg-slate-200': i === store.activeCellCoordinates.cellIndex,
+            'bg-blue-300': store.selectedCells.start.cellIndex <= i && store.selectedCells.end.cellIndex >= i,
+            'bg-slate-300 z-50': store.sheet.style.cols?.[i]?.frozen,
+        }" :style="(store.sheet.style.cols?.[i]?.frozen ? `left: ${store.getFrozenLeft(i)}px;` : '')"
+        @click="store.selectColumn(i)">
         <button
             class="absolute items-center justify-center p-0.5 left-1 hidden rounded-full group-hover:flex hover:bg-slate-300"
-            title="Insert column left" @click="store.addColumn(i - 1)">
+            title="Insert column left" @click="store.addColumn(i)">
             <IconPlus :size="10" />
         </button>
 
-        {{ indexToColumn(i - 1) }}
+        {{ indexToColumn(i) }}
 
         <Dropdown class="absolute right-1 !flex items-center justify-center">
             <template #trigger>
@@ -28,42 +28,42 @@
             </template>
 
             <div class="flex flex-col text-sm w-52">
-                <div class="p-2 font-semibold border-b text-md">Column {{ indexToColumn(i - 1) }}</div>
+                <div class="p-2 font-semibold border-b text-md">Column {{ indexToColumn(i) }}</div>
                 <div class="flex items-center gap-2 p-2">
                     <IconArrowAutofitWidth :size="16" />
                     <div class="flex-1 font-semibold">Width</div>
                     <div class="flex items-baseline gap-1">
                         <input type="number" min="40" max="2500" class="p-1 border rounded outline-none"
-                            :value="store.sheet.style.cols?.[i - 1]?.width || '80'"
-                            @change="e => store.updateColStyle(i - 1, { width: parseInt((e.target as HTMLInputElement).value) })">px
+                            :value="store.sheet.style.cols?.[i]?.width || '80'"
+                            @change="e => store.updateColStyle(i, { width: parseInt((e.target as HTMLInputElement).value) })">px
                     </div>
                 </div>
                 <div class="flex items-center gap-2 p-2 border-t">
-                    <input :id="'frozen-col-' + i" :checked="store.sheet.style.rows?.[i - 1]?.frozen" type="checkbox"
+                    <input :id="'frozen-col-' + i" :checked="store.sheet.style.rows?.[i]?.frozen" type="checkbox"
                         class="w-4 accent-slate-600"
-                        @change="e => store.updateColStyle(i - 1, { frozen: (e.target as HTMLInputElement).checked })">
+                        @change="e => store.updateColStyle(i, { frozen: (e.target as HTMLInputElement).checked })">
                     <label :for="'frozen-col-' + i" class="font-semibold cursor-pointer select-none">Freeze</label>
                 </div>
                 <div class="flex items-center gap-2 p-2 font-semibold border-t text-slate-400"
                     :class="{ 'text-slate-900 cursor-pointer hover:bg-slate-100': i > 1 }"
-                    @click="store.moveColumnLeft(i - 1)">
+                    @click="store.moveColumnLeft(i)">
                     <IconArrowMoveLeft :size="16" /> Move left
                 </div>
                 <div class="flex items-center gap-2 p-2 font-semibold border-b text-slate-400"
                     :class="{ 'text-slate-900 cursor-pointer hover:bg-slate-100': i < store.columnCount - 1 }"
-                    @click="store.moveColumnRight(i - 1)">
+                    @click="store.moveColumnRight(i)">
                     <IconArrowMoveRight :size="16" /> Move right
                 </div>
                 <div class="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-slate-100"
-                    @click="store.addColumn(i - 1)">
+                    @click="store.addColumn(i)">
                     <IconColumnInsertLeft :size="16" /> Insert column left
                 </div>
                 <div class="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-slate-100"
-                    @click="store.addColumn(i)">
+                    @click="store.addColumn(i + 1)">
                     <IconColumnInsertRight :size="16" /> Insert column right
                 </div>
                 <div class="flex items-center gap-2 p-2 font-semibold border-t cursor-pointer hover:bg-slate-100"
-                    @click="store.deleteColumn(i - 1)">
+                    @click="store.deleteColumn(i)">
                     <IconColumnRemove :size="16" /> Delete column
                 </div>
             </div>
