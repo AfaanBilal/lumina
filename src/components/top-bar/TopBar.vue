70c54fc252cb6c64ba3691e2fdb4ad92dc8f0bf7
Afaan Bilal <afaanbilal@gmail.com>
2023-12-19 09:21:18 +0530

    ✨ Top Bar: Redesign

--- src/components/top-bar/TopBar.vue
+++ src/components/top-bar/TopBar.vue
@@ -2,22 +2,20 @@
     <div class="flex flex-col">
         <div class="flex flex-wrap items-center justify-between p-2">
             <div class="flex items-center gap-2">
-                <img src="/logo.png" alt="logo" class="w-8">
-                <h1 class="text-2xl font-semibold">Lumina</h1>
+                <img src="/logo.png" alt="logo" class="w-6">
+                <h1 class="text-xl font-semibold">Lumina</h1>
             </div>
-            <div class="flex flex-col gap-1">
-                <div class="flex justify-end gap-2 pb-2 text-sm border-b">
-                    <a href="https://github.com/AfaanBilal/lumina" target="_blank" rel="noopener"
-                        class="flex items-center gap-1 pr-2 text-xs border-r print:hidden border-r-slate-300 text-slate-500 hover:text-blue-500">
-                        <IconBrandGithub :size="16" /> GitHub
-                    </a>
-                    <a href="https://afaan.dev" target="_blank" rel="noopener"
-                        class="flex items-center gap-1 text-xs text-slate-500 hover:text-blue-500">
-                        <IconCopyright :size="16" /> Afaan Bilal <span class="hidden print:flex">(afaan.dev)</span>
-                    </a>
-                </div>
+            <div class="flex items-center self-start justify-end gap-2 text-sm">
                 <input v-model="filename" type="text" title="Filename"
                     class="px-1 text-sm font-semibold text-right border border-transparent rounded-sm outline-none hover:border-slate-300 focus:border-slate-400 focus:text-left">
+                <a href="https://github.com/AfaanBilal/lumina" target="_blank" rel="noopener"
+                    class="flex items-center gap-1 px-2 text-xs border-x print:hidden text-slate-500 hover:text-blue-500">
+                    <IconBrandGithub :size="16" /> GitHub
+                </a>
+                <a href="https://afaan.dev" target="_blank" rel="noopener"
+                    class="flex items-center gap-1 text-xs text-slate-500 hover:text-blue-500">
+                    <IconCopyright :size="16" /> Afaan Bilal <span class="hidden print:flex">(afaan.dev)</span>
+                </a>
             </div>
         </div>
         <TopBanner />
